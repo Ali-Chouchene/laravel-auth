72f5bee6aea9260f14ea3532411c2e1683958af3
Ali-Chouchene <choucheneali9801@gmail.com>
2023-03-07 16:23:26 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div>
+        <h1>
+            HI, I'M ALI CHOUCHENE
+        </h1>
+        <h3>
+            A JR. Web Developer .....
+        </h3>
+    </div>
+</div>
+
+@endsection
